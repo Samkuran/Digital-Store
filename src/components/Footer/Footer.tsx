b515a548c9bcee978b98c1e141853444a2c3691d
Samantha <samthvet@hotmail.com>
2024-07-20 19:19:26 -0300

    Start structure

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,20 @@
-export function Footer(){
-    return(
-        <footer className="bg-darkgray text-white">
-            eu sou o Footer
-        </footer>
-    )
+export function Footer() {
+  return (
+    <footer className="bg-darkgray text-white px-5 pb-11">
+      <div>
+        <div>logo</div>
+        <div>informações</div>
+        <div>
+          <h4 className="text-lg font-semibold">Contato</h4>
+          <p className="mr-14 mb-11 mt-3">
+            Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE,
+            60150-161
+          </p>
+          <p>(85) 3051-3411</p>
+        </div>
+      </div>
+      <hr className="opacity-30 mt-10 mb-5" />
+      <div className="text-xs text-center">@ 2022 Digital College</div>
+    </footer>
+  );
 }
